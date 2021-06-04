8b58e9efd655db99e1ab7d2ac6331d0319b41921
Beyza Hatip <beyzahatip17@gmail.com>
2021-06-05 00:18:35 +0300

    Initial commit

---
+++ config/initializers/social_share_button.rb
@@ -0,0 +1,3 @@
+SocialShareButton.configure do |config|
+  config.allow_sites = %w(twitter facebook  google_bookmark pinterest email linkedin reddit hacker_news telegram  whatsapp_app whatsapp_web)
+end
